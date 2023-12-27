88c4c2ff531931a9c44e58234db011f48ac4e75f
E1675ka <jacobangel254@gmail.com>
2023-12-27 18:08:01 +0300

    connected to db

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,15 @@
 import express from "express";
-
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connetced to MongoDB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 const app = express();
 
 app.listen(5000, () => {
