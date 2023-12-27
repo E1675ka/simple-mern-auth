ee47fad32755b4a9d3344d40a7b05b8aaf264080
E1675ka <jacobangel254@gmail.com>
2023-12-27 20:24:25 +0300

    Create Api routes

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRoutes from "./routes/user.route.js";
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -15,3 +16,4 @@ const app = express();
 app.listen(5000, () => {
   console.log("server listening on port 5000! ");
 });
+app.use("/api/user", userRoutes);
